d2164c617ba5c5f6ba0a041a2c2dbf4d5ab0b19d
Daniel <daniel.moderiano@outlook.com>
2020-07-06 17:45:48 +0930

    Added complete base layout for calculator (unlabelled) using combinations of flexbox and CSS grid

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// Requires arithmetic including add, subtract, multiply and divide. 
+
+// Function operate that accepts 3 parameters: operator, and 2 operands. It then performs the arithmetic using the operands and the operator. 
+
+const operatorFunctions = {
+    '+' : function(a, b) {
+        return a + b;
+    },
+    '-' : function(a, b) {
+        return a - b;
+    },
+    '*' : function(a, b) {
+        return a * b;
+    },
+    '/' : function(a, b) {
+        return a / b;
+    }
+}
+
